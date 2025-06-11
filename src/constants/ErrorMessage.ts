17c60c7e3afc9186d1f69dca897fb47b0c131ae0
Akhil <manojakhil36@gmail.com>
2025-06-12 01:00:56 +0530

    constants files added

---
+++ src/constants/ErrorMessage.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+
+
+    USER_NOT_FOUND: "User not found",
+    INVALID_CREDENTIALS: "Invalid email or password",
+    USER_ALREADY_EXISTS: "User already  exists",
+}
