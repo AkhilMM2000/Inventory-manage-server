2b763723f085a3c29f62d3266856d8e043768ecb
Akhil <manojakhil36@gmail.com>
2025-06-17 12:17:44 +0530

    cookies setup for https

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -31,7 +31,7 @@ export class UserController {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
